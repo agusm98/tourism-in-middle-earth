25c0a72d17955b5432e5f4a7cb8f5ff3512b53d6
agusm98 <agus-e-m1@hotmail.com>
2023-06-08 22:08:28 -0300

    Version inicial clase Ticket

---
+++ src/com/unlam/paradigms/tp/Ticket.java
@@ -0,0 +1,27 @@
+package com.unlam.paradigms.tp;
+
+public class Ticket {
+    private User user;
+    private Offer offer;
+    
+    public Ticket(User user, Offer offer) {
+        this.user = user;
+        this.offer = offer;
+        //this.setTotalAmount();
+        //this.setTotalHours();
+    }
+    
+    /*private void setTotalAmount() {
+        totalAmount = 0;
+        for(Offer offer : this.offers) {
+            this.totalAmount += offer.getPrice();
+        }
+    }*/
+    
+    public void printDetail() {
+        System.out.println("Usuario: "+user.getUserName());
+        System.out.println("Compro: "+offer.getName());
+        System.out.println("Al precio de: "+offer.getPrice());
+        System.out.println("Debera tener disponible: "+offer.getHours()+" horas.");
+    }
+}
