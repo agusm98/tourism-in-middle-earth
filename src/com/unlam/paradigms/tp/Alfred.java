43514c3950e528ffc3604e55d65b7daafcec7dab
agusm98 <agus-e-m1@hotmail.com>
2023-06-10 22:35:52 -0300

    Fix Alfred methods call and datatypes

--- src/com/unlam/paradigms/tp/Alfred.java
+++ src/com/unlam/paradigms/tp/Alfred.java
@@ -1,5 +1,6 @@
 package com.unlam.paradigms.tp;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Iterator;
 import java.util.Scanner;
 
@@ -11,51 +12,50 @@ public class Alfred {
         this.inputScan = new Scanner(System.in);
     }
     
-    public ArrayList<Ticket> offerAttractions(User usr) {
-        ArrayList<Ticket> tickets = new ArrayList<Ticket>();
-        Iterator<Offer> offers = Manager.getOffers(usr); //Offer iterator
+    public List<Ticket> offerAttractions(User usr) {
+        List<Ticket> tickets = new ArrayList<Ticket>();
+        Iterator<TourismOption> tourOptions = Manager.getOptions(usr); //Offer iterator
 
-        while(offers.hasNext()) {
-            Offer offer = offers.next(); //if(offer.isValid(usr))
-            //Mostrar data de oferta
-            if(offer.isPackage()) {
+        while(tourOptions.hasNext()) {
+            TourismOption tourOption = tourOptions.next();
+            //SUG: tourOption.showDescription()
+            if(tourOption.isOffer()) {
                 System.out.println("Promocion");
-                System.out.println("Atracciones incluidas: "+offer.getName());
-                System.out.println("Duracion: "+String.valueOf(offer.getHours()));
-                System.out.println("Precio original: "+String.valueOf(offer.getPrice()));
-                System.out.println("Precio con descuento: "
-                +String.valueOf(offer.getPrice() - offer.getDiscount()));
+                System.out.println("Atracciones incluidas: "+tourOption.getName());
+                System.out.println("Duracion: "+String.valueOf(tourOption.getDuration()));
+                System.out.println("Precio original: "+String.valueOf(tourOption.getBaseAmount()));
+                System.out.println("Precio con descuento: "+String.valueOf(tourOption.getAmountToPay()));
             } else {
                 System.out.println("Atraccion");
-                System.out.println("Nombre: "+offer.getName());
-                System.out.println("Duracion: "+String.valueOf(offer.getHours()));
-                System.out.println("Precio: "+String.valueOf(offer.getPrice()));
+                System.out.println("Nombre: "+tourOption.getName());
+                System.out.println("Duracion: "+String.valueOf(tourOption.getDuration()));
+                System.out.println("Precio: "+String.valueOf(tourOption.getBaseAmount()));
             }
             
             String usrResp = inputScan.nextLine();
             if(usrResp.toUpperCase().contains("S")) {
                 /*
                 MANAGER JOB
-                offer.reserve(usr.name/usr.id);
+                tourOption.reserve(usr.name/usr.id);
                 usr.updateHours();
                 usr.updateGold();
                 Ticket ticket = new Ticket(usr, offer);
                 */
-                Ticket ticket = Manager.createTicket(usr, offer);
+                Ticket ticket = Manager.createTicket(usr, tourOption);
                 tickets.add(ticket);
             }
         }
         return tickets;
     }
     
-    public void showSchedule(ArrayList<Ticket> tickets){
+    public void showSchedule(List<Ticket> tickets){
         Integer totalHours = 0;
         Integer totalGold = 0;
         
         if(tickets.size() > 0){
             for(Integer i=0;i<tickets.size();++i) {
-                totalHours += tickets.get(i).getHours();
-                totalGold += tickets.get(i).getPrice();
+                totalHours += tickets.get(i).getDuration();
+                totalGold += tickets.get(i).getTotalAmount();
                 System.out.println("Excursion "+String.valueOf(i)+":/n");
                 tickets.get(i).printDetail();
             }
