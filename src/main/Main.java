024c04518edeeb41c7fd83116278e7b19f55a909
thomascura-py <thomascura1998@gmail.com>
2023-06-11 21:32:00 -0300

    main start Alfred

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,22 +1,32 @@
 package main;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.unlam.paradigms.tp.User;
 import com.unlam.paradigms.tp.UserLoader;
+import com.unlam.paradigms.tp.Alfred;
+import com.unlam.paradigms.tp.Ticket;
 
 public class Main {
 	public static void main(String[] args) {
 		String fileName = "tourism-in-middle-earth/src/source-data/users.txt";
 		File file = new File(fileName);
 		String absolutePath = file.getAbsolutePath();
-		
 
 		UserLoader userLoader = new UserLoader(absolutePath);
-		
+
+		Alfred alfred = new Alfred();
+
+		// Manager manager = new Manager();
+
 		List<User> users = new ArrayList<User>();
-		
+
+		List<Ticket> tickets = new ArrayList<Ticket>();
+
+		List<Ticket> totalTickets = new ArrayList<Ticket>();
+
 		try {
 			users = userLoader.processAndParse();
 		} catch (Exception e) {
@@ -24,7 +34,16 @@ public class Main {
 		}
 
 		for (User user : users) {
-			System.out.println(user.getUserName()+"--"+user.getBudget()+"--"+user.getAvailableHours()+"--"+user.getTouristAttraction());
+			System.out.println(user.getUserName() + "--" + user.getBudget() + "--" + user.getAvailableHours() + "--"
+					+ user.getTouristAttraction());
+			tickets = alfred.offerAttractions(user);
+
+			alfred.showSchedule(tickets);
+
+			totalTickets.addAll(tickets);
 		}
+
+		// manager.generateFile(totalTickets);
+
 	}
 }
